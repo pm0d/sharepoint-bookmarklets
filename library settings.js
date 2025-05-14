f4525bbd61bcf84297a7564feb896fdc3bfee07f
Pramod Ghuge <20861876+pm0d@users.noreply.github.com>
2025-05-14 16:58:48 +0530

    Create library settings.js

---
+++ library settings.js
@@ -0,0 +1,8 @@
+javascript:document.getElementById('O365_MainLink_Settings').click();setTimeout(()=>{document.getElementById('SuiteMenu_LibrarySettings').click();},200);
+
+// Bookmarklet to quickly open library settings for the current library
+//javascript:
+//document.getElementById('O365_MainLink_Settings').click();
+//setTimeout(() => {
+//    document.getElementById('SuiteMenu_LibrarySettings').click();
+//}, 200);
